31dc0046ceb99055540f08c8fd6453ac8f5fed7a
Ladislav Navratil <navratill@gmail.com>
2022-01-25 22:57:28 +0100

    Initial Commit

---
+++ sdk-ios-example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sdk-ios-example
+//
+//  Created by Ladislav Navratil on 25.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sdk-ios-example/sdk_ios_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sdk_ios_exampleApp.swift
+//  sdk-ios-example
+//
+//  Created by Ladislav Navratil on 25.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct sdk_ios_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
